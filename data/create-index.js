4f7f3a87719052c4e9b4dcc802ed5efba4ebcf5b
Kyle Parkinson <kyle.parkinson@mongodb.com>
2024-05-07 16:26:21 -0700

    add create index script

---
+++ data/create-index.js
@@ -0,0 +1,8 @@
+
+use('education')
+// Connect to the specific database
+if(!db.grades.getIndexes().some(index == index.key["date_completed"] === 1)) {
+  db.grades.createIndex({ "date_completed": 1 })
+}
+
+
